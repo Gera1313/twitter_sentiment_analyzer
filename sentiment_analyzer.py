65abd74186c4b8a9dce97f1a05bedbff756e355d
Mark Anthony <touya1313@hotmail.com>
2024-10-31 09:07:19 -0600

    access token

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -4,4 +4,4 @@ import os
 api_key = os.getenv('api_key_here')
 api_secret = os.getenv('secret_api_here')
 access_token = os.getenv('access_token_here')
-access_token_secret =
+access_token_secret = os.getenv('secret_access_token_here')
