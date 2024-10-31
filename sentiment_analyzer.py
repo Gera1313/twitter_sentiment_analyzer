798c706d6528b8dc755ade7f73b6a2ef3f66c4b8
Mark Anthony <touya1313@hotmail.com>
2024-10-31 09:06:33 -0600

    notes

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -3,5 +3,5 @@ import os
 # Get the API keys from environment variables
 api_key = os.getenv('api_key_here')
 api_secret = os.getenv('secret_api_here')
-access_token =
+access_token = os.getenv('access_token_here')
 access_token_secret =
