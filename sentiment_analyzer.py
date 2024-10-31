797f9264e87a69f17757421b64c8e1a788f20069
Mark Anthony <touya1313@hotmail.com>
2024-10-31 09:09:15 -0600

    function missing keys

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -7,3 +7,5 @@ access_token = os.getenv('access_token_here')
 access_token_secret = os.getenv('secret_access_token_here')
 
 # Check if all variables were loaded correctly
+if not all([api_key, api_secret, access_token, access_token_secret]):
+    raise ValueError("One or more environment variables for Twitter API keys are missing.")
