e9dda989c6de9262523be3bc325ea23c4caefcdc
Mark Anthony <touya1313@hotmail.com>
2024-10-31 09:08:10 -0600

    new function

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -5,3 +5,5 @@ api_key = os.getenv('api_key_here')
 api_secret = os.getenv('secret_api_here')
 access_token = os.getenv('access_token_here')
 access_token_secret = os.getenv('secret_access_token_here')
+
+# Check if all variables were loaded correctly
