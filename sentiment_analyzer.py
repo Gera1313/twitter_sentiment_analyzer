310dce2ee7f83921239a25ffde8fb0ab3af67177
Mark Anthony <touya1313@hotmail.com>
2024-11-02 23:54:50 -0500

    twitter api

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -4,8 +4,8 @@ import os
 # Get the API keys from environment variables
 load_dotenv()
 
-api_key = os.getenv('api_key_here')
-api_secret = os.getenv('secret_api_here')
+api_key = os.getenv('TWITTER_API_KEY')
+api_secret = os.getenv('TWITTER_API_SECRET')
 access_token = os.getenv('access_token_here')
 access_token_secret = os.getenv('secret_access_token_here')
 
